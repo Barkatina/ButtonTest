d336928ada453b563aba54a3e08da715ecf5409a
Barkatina <159892946+Barkatina@users.noreply.github.com>
2024-02-22 12:27:58 +0300

    Initial commit

---
+++ src/test/java/com/example/buttontest/MainPageTest.java
@@ -0,0 +1,43 @@
+package com.example.buttontest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demoqa.com/dynamic-properties");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void enable() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement disableButton = driver.findElement(By.cssSelector(("#visibleAfter")));
+        wait.until(ExpectedConditions.elementToBeClickable(disableButton));
+        assertTrue(disableButton.isEnabled(), "Кнопка не стала активна");
+    }
+}
